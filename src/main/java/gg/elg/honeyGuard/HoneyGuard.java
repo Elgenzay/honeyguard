356005f0def153b6629e5ba656e918074b651583
Jason <jason@elg.gg>
2024-09-28 11:48:00 -0400

    Initial commit

---
+++ src/main/java/gg/elg/honeyGuard/HoneyGuard.java
@@ -0,0 +1,17 @@
+package gg.elg.honeyGuard;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HoneyGuard extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
